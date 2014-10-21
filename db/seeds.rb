286895e77715c4464faeb3ee1d4260abab0ce0d9
melliott <mcelliott80@gmail.com>
2014-10-22 09:26:33 +1100

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,17 @@ user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
 
 ['Matt', 'Giedre', 'Barb', 'Mark', 'Mike', 'Holly'].each do |flyer|
-  user.flyers.create(name: flyer, hours: 50)
+  user.flyers.create(name: flyer, hours: 50, user: user)
 end
 
-['Head Up', 'Head Down', 'Belly'].each do |category|
-  user.categories.create(name: category)
+['Head Down', 'Head Up', 'Belly'].each do |category|
+  user.categories.create(name: category, user: user)
+end
+
+event = Event.create(name: 'Freefly', location: 'ifly', event_date: 1.month.from_now, team_size: 2, user: user )
+
+
+Flyer.all.each_with_index do |flyer, index|
+  p = event.participants.build(user: user, flyer: flyer, category: Category.first, number: index+1)
+  p.save!
 end
