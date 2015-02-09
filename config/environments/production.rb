802928ca1e4804a47e08a58d91840ce0c008145d
melliott <mcelliott80@gmail.com>
2015-02-09 17:51:36 +1100

    prod logger

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,8 +41,10 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
+  config.logger = Logger.new(STDOUT)
   # Set to :debug to see everything in the log.
   config.log_level = :info
+
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
