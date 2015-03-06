20ea7687cdf67c753d25daefc2a8e8cd257ec376
melliott <mcelliott80@gmail.com>
2015-03-06 21:12:20 +1100

    update flyer factory, update tunnel hours

--- app/enumerations/tunnel_hours.rb
+++ app/enumerations/tunnel_hours.rb
@@ -1,9 +1,9 @@
 class TunnelHours < EnumerateIt::Base
   associate_values(
-      beginner:    [1, "0 - 10 hours"],
-      novice:      [2, "11 - 50 hours"],
-      experienced: [3, "51 - 100 hours"],
-      expert:      [4, "101 - 200 hours"],
-      ninja:       [5, "201 + hours"]
+      beginner:    [0, "0 - 10 hours"],
+      novice:      [1, "11 - 50 hours"],
+      experienced: [2, "51 - 100 hours"],
+      expert:      [3, "101 - 200 hours"],
+      ninja:       [4, "201 + hours"]
   )
 end
--- spec/factories/flyers.rb
+++ spec/factories/flyers.rb
@@ -4,7 +4,7 @@ FactoryGirl.define do
   factory :flyer do
     sequence(:name)  { |n| "Flyer #{n}" }
     sequence(:email) { |n| "flyer_#{n}@foo.com" }
-    hours 50
+    hours 1
     user
   end
 end
