3ef352eb865bda38cd92c586043e790c971705c2
melliott <mcelliott80@gmail.com>
2015-04-25 20:43:50 +1000

    update participant spec

--- spec/models/participant_spec.rb
+++ spec/models/participant_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe Participant do
-  it { should respond_to :flyer_name  }
+  it { should respond_to :name  }
   it { should respond_to :category    }
   it { should respond_to :flyer       }
   it { should respond_to :event       }
