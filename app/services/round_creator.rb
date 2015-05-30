92e0d9c31fc295410b51d01d739af6869b934416
melliott <mcelliott80@gmail.com>
2015-05-30 23:42:49 +1000

    working round generator

--- app/services/round_creator.rb
+++ app/services/round_creator.rb
@@ -1,14 +1,14 @@
 class RoundCreator
   def initialize(event, round_number)
     @event = event
-    @round_number = round_number
+    @round_number = round_number.to_i
   end
 
   def perform
     Round.create(
       event: @event,
-      round_number: @round_number + 1,
-      name: "Round #{@round_number + 1}"
+      round_number: @round_number,
+      name: "Round #{@round_number}"
     )
   end
 end
